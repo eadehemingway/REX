959bab50329956f510bdeb353f03580a131dc36f
eadehemingway <eade.hemingway@gmail.com>
2019-07-22 18:24:43 +0300

    build the send rex modal

---
+++ public/components/ProfilePage/Modal.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export class Modal extends React.Component {
+  render() {
+    return (
+      <div className="modal-overlay">
+        <div className="modal-window">
+          {' '}
+          SEND RECOMMENDATION
+          <div className="modal-content">
+            <input type="text" placeholder="film name" />
+            <input type="text" placeholder="rex handle" />
+            <button>send</button>
+            <button onClick={this.props.toggleModal}>close</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
