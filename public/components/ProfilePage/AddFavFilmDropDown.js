48c77a6af497971d9f90736b2d6882a8fe0a1300
eadehemingway <eade.hemingway@gmail.com>
2019-07-23 12:04:19 +0300

    add fav film drop down

--- public/components/ProfilePage/AddFavFilmDropDown.js
+++ public/components/ProfilePage/AddFavFilmDropDown.js
@@ -1,6 +1,4 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Modal } from './Modal';
+import React from 'react';
 
 export class AddFavFilmDropDown extends React.Component {
   state = {
--- public/components/ProfilePage/FavouriteFilms.js
+++ public/components/ProfilePage/FavouriteFilms.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Modal } from './Modal';
+import React from 'react';
+
 import { AddFavFilmDropDown } from './AddFavFilmDropDown';
 
 export class FavouriteFilms extends React.Component {
