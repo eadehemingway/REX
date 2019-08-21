5265a2dd1102bb770cab41675c1a3c00f0c7320e
eadehemingway <eade.hemingway@gmail.com>
2019-08-21 11:53:31 +0300

    make handlers consistently get the username from cookies

--- server/controllers/rex.js
+++ server/controllers/rex.js
@@ -1,41 +1,44 @@
+const cookie = require('cookie')
+
 exports.addRex = (req, res) => {
-  const reqInfo = req.body.rex;
-  const handle = req.body.toHandle; // in the body have the handle of who you want to send rec to.
+  const reqInfo = req.body.rex
+  const handle = req.body.toHandle // in the body have the handle of who you want to send rec to.
   User.findOne({ handle: handle }, (err, doc) => {
-    if (err) console.log('err', err);
-    doc.receivedRex.push(reqInfo);
-    doc.save();
-    res.json({ doc });
-  });
-};
+    if (err) console.log('err', err)
+    doc.receivedRex.push(reqInfo)
+    doc.save()
+    res.json({ doc })
+  })
+}
 
 exports.deleteRex = (req, res) => {
-  const { id } = req.params;
-  const handle = 'eade'; // get from cookies
+  const { id } = req.params
+  const handle = cookie.parse(req.headers.cookie).user
+
   User.findOne({ handle }, (err, doc) => {
-    if (err) console.log('err', err);
+    if (err) console.log('err', err)
 
-    const rexArr = doc.receivedRex;
-    const index = rexArr.findIndex(e => e.id === id);
-    rexArr.splice(index, 1);
+    const rexArr = doc.receivedRex
+    const index = rexArr.findIndex(e => e.id === id)
+    rexArr.splice(index, 1)
 
-    doc.save();
-    res.json({ doc });
-  });
-};
+    doc.save()
+    res.json({ doc })
+  })
+}
 
 exports.changeRexStatus = (req, res) => {
-  const { id } = req.params;
-  const { pendingStatus } = req.body;
-  const handle = 'eade'; // get from cookies
+  const { id } = req.params
+  const { pendingStatus } = req.body
+  const handle = cookie.parse(req.headers.cookie).user
   User.findOne({ handle }, (err, doc) => {
-    if (err) console.log('err', err);
+    if (err) console.log('err', err)
 
-    const rexArr = doc.receivedRex;
-    const index = rexArr.findIndex(e => e.id === id);
-    rexArr[index].pending = pendingStatus;
+    const rexArr = doc.receivedRex
+    const index = rexArr.findIndex(e => e.id === id)
+    rexArr[index].pending = pendingStatus
 
-    doc.save();
-    res.json({ doc });
-  });
-};
+    doc.save()
+    res.json({ doc })
+  })
+}
