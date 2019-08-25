c72018da37f1399e0f85e3be1b72ba14a9c94ce7
eadehemingway <eade.hemingway@gmail.com>
2019-08-25 15:54:20 +0300

    add background rectangle

--- public/src/App.js
+++ public/src/App.js
@@ -5,21 +5,24 @@ import { Signup } from './components/Signup'
 import { SigninConnected } from './components/Signin'
 import { ProtectedRoute } from './authRoute'
 import { ProfilePageConnected } from './components/ProfilePage'
-
+import './App.css'
 import { LandingPage } from './components/LandingPage'
 
 export class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <NavWithRouter />
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/signin" component={SigninConnected} />
-          <ProtectedRoute path="/user/:id" component={ProfilePageConnected} />
-        </Switch>
-      </BrowserRouter>
+      <div className="App">
+        <BrowserRouter>
+          <NavWithRouter />
+          <div className="background-rect"></div>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/signin" component={SigninConnected} />
+            <ProtectedRoute path="/user/:id" component={ProfilePageConnected} />
+          </Switch>
+        </BrowserRouter>
+      </div>
     )
   }
 }
