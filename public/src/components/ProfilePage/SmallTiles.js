766791fbdc9537025c77beb166df9129abfd2e61
eadehemingway <eade.hemingway@gmail.com>
2019-08-26 14:22:30 +0300

    make delete buttons work for small tiles

--- public/src/components/ProfilePage/SmallTiles.js
+++ public/src/components/ProfilePage/SmallTiles.js
@@ -16,7 +16,7 @@ export class SmallTiles extends React.Component {
     this.props.addFilm(e)
   }
   render() {
-    const { films, editMode } = this.props
+    const { films, editMode, deleteFilm, openModal } = this.props
     const { addFilmPanelOpen } = this.state
     return (
       <div className="small-tile-section">
@@ -25,11 +25,18 @@ export class SmallTiles extends React.Component {
         {addFilmPanelOpen && <AddFavFilm addFilm={this.addFilm} />}
         <div className="small-tile-container">
           {films.map((f, i) => (
-            <div className="cropped-to-square" key={i}>
+            // <div className="cropped-to-square" key={i}>
+            <div key={i}>
               <img
                 className="small-film-img"
                 src={`https://image.tmdb.org/t/p/w185/${f.poster_path}`}
               />
+              {editMode && (
+                <button onClick={() => deleteFilm(f._id)}>delete</button>
+              )}
+              <button onClick={() => openModal({ title, poster_path })}>
+                recommend this film
+              </button>
             </div>
           ))}
         </div>
