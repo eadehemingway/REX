a2cc02c449d2f95e8086b283bbbc52553870efa5
eadehemingway <eade.hemingway@gmail.com>
2019-08-26 15:17:36 +0300

    make buttons show on all tiles

--- public/src/components/ProfilePage/FilledLargeTile.js
+++ public/src/components/ProfilePage/FilledLargeTile.js
@@ -6,7 +6,7 @@ export class FilledLargeTile extends React.Component {
     const { film, editMode, deleteFilm, openModal } = this.props
 
     return (
-      <div>
+      <div className="large-film-tile">
         <img
           className="large-film-img"
           src={`https://image.tmdb.org/t/p/w185/${film.poster_path}`}
@@ -18,11 +18,19 @@ export class FilledLargeTile extends React.Component {
                   {t.name}
                 </p>
               ))} */}
-
-        {editMode && (
-          <button onClick={() => deleteFilm(film._id)}>delete</button>
-        )}
-        <button onClick={() => openModal(film)}>recommend this film</button>
+        <div className="onhover-tile-btn-container">
+          {editMode && (
+            <button
+              className="onhover-tile-btn"
+              onClick={() => deleteFilm(film._id)}
+            >
+              delete
+            </button>
+          )}
+          <button className="onhover-tile-btn" onClick={() => openModal(film)}>
+            recommend this film
+          </button>
+        </div>
       </div>
     )
   }
--- public/src/components/ProfilePage/SmallTiles.js
+++ public/src/components/ProfilePage/SmallTiles.js
@@ -4,11 +4,18 @@ import { AddFavFilm } from './AddFavFilm'
 
 export class SmallTiles extends React.Component {
   state = {
-    addFilmPanelOpen: false
+    addFilmPanelOpen: false,
+    showTileButtons: true
   }
   toggleAddPanel = () => {
     this.setState(prevState => ({
       addFilmPanelOpen: !prevState.addFilmPanelOpen
+    }))
+  }
+  toggleShowTileBtns = f => {
+    console.log('tooooooooooooge')
+    this.setState(prevState => ({
+      showTileButtons: !prevState.showTileButtons
     }))
   }
   addFilm = e => {
@@ -17,7 +24,7 @@ export class SmallTiles extends React.Component {
   }
   render() {
     const { films, editMode, deleteFilm, openModal } = this.props
-    const { addFilmPanelOpen } = this.state
+    const { addFilmPanelOpen, showTileButtons } = this.state
     return (
       <div className="small-tile-section">
         <h2>SMALL TILES</h2>
@@ -25,16 +32,29 @@ export class SmallTiles extends React.Component {
         {addFilmPanelOpen && <AddFavFilm addFilm={this.addFilm} />}
         <div className="small-tile-container">
           {films.map((f, i) => (
-            // <div className="cropped-to-square" key={i}>
-            <div key={i}>
+            <div className="cropped-to-square" key={i}>
               <img
                 className="small-film-img"
                 src={`https://image.tmdb.org/t/p/w185/${f.poster_path}`}
               />
-              {editMode && (
-                <button onClick={() => deleteFilm(f._id)}>delete</button>
+              {showTileButtons && (
+                <div className="onhover-tile-btn-container">
+                  {editMode && (
+                    <button
+                      className="onhover-tile-btn"
+                      onClick={() => deleteFilm(f._id)}
+                    >
+                      delete
+                    </button>
+                  )}
+                  <button
+                    className="onhover-tile-btn"
+                    onClick={() => openModal(f)}
+                  >
+                    recommend this film
+                  </button>
+                </div>
               )}
-              <button onClick={() => openModal(f)}>recommend this film</button>
             </div>
           ))}
         </div>
