257fa68439780457d5f682a612bacad520ca1060
eadehemingway <eade.hemingway@gmail.com>
2019-08-27 13:30:12 +0300

    make films show in tag groups

--- public/src/components/ProfilePage/SmallTiles.js
+++ public/src/components/ProfilePage/SmallTiles.js
@@ -5,7 +5,25 @@ import { FilmTile } from './FilmTile'
 export class SmallTiles extends React.Component {
   state = {
     addFilmPanelOpen: false,
-    showMoreMenu: false
+    showMoreMenu: false,
+    tags: null,
+    films: null
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.films !== prevState.films) {
+      const { films } = this.props
+      const tags = films.reduce((acc, el) => {
+        const tagNames = acc.map(t => t.name)
+        el.tag.forEach(t => {
+          if (!tagNames.includes(t.name)) {
+            acc.push(t)
+          }
+        })
+        return acc
+      }, [])
+
+      this.setState({ tags, films })
+    }
   }
   toggleAddPanel = () => {
     this.setState(prevState => ({
@@ -21,8 +39,11 @@ export class SmallTiles extends React.Component {
     this.props.addFilm(e)
   }
   render() {
-    const { films, editMode, deleteFilm, openModal } = this.props
-    const { addFilmPanelOpen, showMoreMenu } = this.state
+    const { editMode, deleteFilm, openModal } = this.props
+    const { addFilmPanelOpen, showMoreMenu, tags, films } = this.state
+    const filmsWithNoTag = (films || []).filter(f => {
+      return f.tag.length === 0
+    })
     return (
       <div className="small-tile-section">
         <h2>SMALL TILES</h2>
@@ -35,28 +56,61 @@ export class SmallTiles extends React.Component {
         )}
 
         <div className="small-tile-container">
-          {films.map((f, i) => {
-            return (
-              <div key={i}>
-                <FilmTile
-                  film={f}
-                  editMode={editMode}
-                  deleteFilm={deleteFilm}
-                  openModal={openModal}
-                  containerClass="cropped-to-square"
-                />
-                {f.tag.length > 0 &&
-                  f.tag.map((t, i) => (
-                    <div
-                      key={i}
-                      style={{ background: t.colour, border: '2px solid grey' }}
-                    >
-                      {t.name}
-                    </div>
-                  ))}
-              </div>
-            )
-          })}
+          {tags &&
+            tags.map((t, i) => {
+              const filmWithThisTag = films.filter(f => {
+                const tagNames = f.tag.map(t => t.name)
+                return tagNames.includes(t.name)
+              })
+              return (
+                <div className="tag-group" key={i}>
+                  <h3
+                    style={{
+                      background: t.colour,
+                      border: '2px solid grey'
+                    }}
+                  >
+                    {t.name}
+                  </h3>
+                  {filmWithThisTag.map((f, i) => {
+                    return (
+                      <div key={i}>
+                        <FilmTile
+                          film={f}
+                          editMode={editMode}
+                          deleteFilm={deleteFilm}
+                          openModal={openModal}
+                          containerClass="cropped-to-square"
+                        />
+                      </div>
+                    )
+                  })}
+                </div>
+              )
+            })}
+          {filmsWithNoTag.length > 0 && (
+            <div className="tag-group">
+              <h3
+                style={{
+                  background: 'lightgrey',
+                  border: '2px solid grey'
+                }}
+              >
+                No Tag
+              </h3>
+              {filmsWithNoTag.map((f, i) => (
+                <div key={i}>
+                  <FilmTile
+                    film={f}
+                    editMode={editMode}
+                    deleteFilm={deleteFilm}
+                    openModal={openModal}
+                    containerClass="cropped-to-square"
+                  />
+                </div>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     )
