ac6513cc065ad513fe89cf7cb435b504eb1a9c12
Jemila <jemila.abulhawa@gmail.com>
2019-10-31 14:42:21 +0000

    change proxy port

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ mongoose
   })
   .then(() => console.log('DB connection successful'))
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`)
